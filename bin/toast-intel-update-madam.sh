da7735c6453d9cd0b0b50861581edf46750fb8df
Kolen Cheung <christian.kolen@gmail.com>
2019-10-08 15:58:32 -0700

    add toast-intel-update-madam.sh

---
+++ bin/toast-intel-update-madam.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# This script pull the lastest master of TOAST and build that again
+# accompany toast-intel.sh
+
+set -e
+
+# customize these
+CONDAMPI=true
+AATMVERSION=0.5
+P=10
+ENVNAME=toast-intel-fftw
+prefix="$SCRATCH/local/$ENVNAME"
+# * assume FFTW from system's package manager
+FFTWPATH=/usr
+
+# for the build directory
+date=$(date +%Y%m%d)
+
+. activate "$prefix"
+# make sure intel paths has higher priorities then conda's
+. /opt/intel/bin/compilervars.sh -arch intel64
+
+# toast ##########################################################################################
+
+# assume you pull the master or checkout whichever branch you like already
+
+cd "$prefix/git"
+cd libmadam
+
+./autogen.sh
+
+FC=ifort \
+MPIFC=mpiifort \
+FCFLAGS="-O3 -g -fPIC -march=native -mtune=native -fexceptions -pthread -heap-arrays 16" \
+CC=icc \
+MPICC=mpiicc \
+CFLAGS="-O3 -g -fPIC -march=native -mtune=native -pthread" \
+./configure \
+    --with-fftw="$FFTWPATH" \
+    --prefix="$prefix"
+
+make -j$P
+make install
+
+. activate "$prefix"
+# make sure intel paths has higher priorities then conda's
+. /opt/intel/bin/compilervars.sh -arch intel64
+
+cd python
+python setup.py install
+python setup.py test
