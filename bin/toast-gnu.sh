6973280a164326aa9baa5c47873d8fef3fd99e9c
Kolen Cheung <christian.kolen@gmail.com>
2019-10-18 16:14:36 -0700

    toast-gnu.sh: 1st time working on linux

--- bin/toast-gnu.sh
+++ bin/toast-gnu.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
 # * using the master libmadam and toast
-# * TBB can't be found in SuiteSparse and TOAST
 
 set -e
 
 # customize these
-CONDAMPI=true
 AATMVERSION=0.5
 P=${P-$(($(getconf _NPROCESSORS_ONLN) / 2))}
-ENVNAME=toast-gnu-fftw
-prefix="$SCRATCH/local/$ENVNAME"
+ENVNAME=toast-gnu
+prefixBase="$SCRATCH/local/$ENVNAME"
+prefixDownload="$prefixBase/git"
+prefixCompile="$prefixBase/compile"
+prefixConda="$prefixBase/conda"
 # * assume FFTW from system's package manager
 FFTWPATH=/usr
-
-mkdir -p "$prefix" && cd "$prefix"
 
 # on macOS try this for dependencies
 # brew install fftw cfitsio suite-sparse
@@ -36,9 +35,14 @@ fi
 MPICC=mpicc
 MPICXX=mpicxx
 
+mkdir -p "$prefixDownload"
+mkdir -p "$prefixCompile"
+mkdir -p "$prefixConda"
+
 # Install Python dep. via conda ##################################################################
 
-# nomkl to force OpenBLAS
+cd "$prefixConda"
+
 cat << EOF > env.yml
 channels:
 - defaults
@@ -51,17 +55,19 @@ dependencies:
 - matplotlib
 - pyephem
 - healpy
-- nomkl
+- 'numba>=0.45'
+- toml
+- cython
+- mypy
+- pylint
+- 'plotly>=4.1'
+- nbformat
 EOF
 
-if [[ -n "$CONDAMPI" ]]; then
-    echo '- conda-forge::mpi4py' >> env.yml
-fi
+conda env create -f env.yml -p "$prefixConda"
+# rm -f env.yml
 
-# conda env create -f env.yml -p "$prefix"
-rm -f env.yml
-
-. activate "$prefix"
+. activate "$prefixConda"
 
 # mpi4py
 # * hardcoded the location of this script for now
@@ -75,8 +81,7 @@ conda deactivate
 
 # AATM ##########################################################################################
 
-mkdir -p "$prefix/git"
-cd "$prefix/git"
+cd "$prefixDownload"
 wget -qO- "https://launchpad.net/aatm/trunk/0.5/+download/aatm-${AATMVERSION}.tar.gz" | tar -xzf -
 cd "aatm-$AATMVERSION"
 
@@ -85,7 +90,7 @@ CXX=$GXX \
 CFLAGS="-O3 -g -fPIC -march=native -mtune=native -pthread" \
 CXXFLAGS="-O3 -g -fPIC -march=native -mtune=native -pthread -std=c++11" \
 ./configure \
-    --prefix="$prefix"
+    --prefix="$prefixCompile"
 
 make -j$P
 make install
@@ -94,7 +99,7 @@ make install
 
 # * assume CFITSIO installed using system's package manager
 
-cd "$prefix/git"
+cd "$prefixDownload"
 git clone git@github.com:hpc4cmb/libmadam.git
 cd libmadam
 
@@ -102,7 +107,7 @@ cd libmadam
 
 if [[ $(uname) == Darwin ]]; then
 LIBRARY_PATH=/usr/local/Cellar/cfitsio/3.450_1/lib \
-LD_LIBRARY_PATH=$prefix/lib \
+LD_LIBRARY_PATH=$prefixCompile/lib \
 FC=gfortran-9 \
 MPIFC=$MPIFORT \
 FCFLAGS="-O3 -fPIC -pthread -march=native -mtune=native" \
@@ -111,7 +116,7 @@ MPICC=$MPICC \
 CFLAGS="-O3 -fPIC -pthread -march=native -mtune=native" \
 ./configure \
     --with-fftw="$FFTWPATH" \
-    --prefix="$prefix"
+    --prefix="$prefixCompile"
 else
 FC=gfortran \
 MPIFC=$MPIFORT \
@@ -121,23 +126,28 @@ MPICC=$MPICC \
 CFLAGS="-O3 -fPIC -pthread -march=native -mtune=native" \
 ./configure \
     --with-fftw="$FFTWPATH" \
-    --prefix="$prefix"
+    --prefix="$prefixCompile"
 fi
 
 make -j$P
 make install
 
-. activate "$prefix"
+. activate "$prefixConda"
 
+# TODO is installing to conda ok?
 cd python
 python setup.py install
 python setup.py test
+
+# conda deactivate
 
 # toast ##########################################################################################
 
 # * assume suite-sparse installed using system's package manager
 
-cd "$prefix/git"
+export LD_LIBRARY_PATH="$prefixCompile/lib:$LD_LIBRARY_PATH"
+
+cd "$prefixDownload"
 git clone git@github.com:hpc4cmb/toast.git
 cd toast
 
@@ -151,13 +161,16 @@ cmake \
     -DMPI_CXX_COMPILER=$MPICXX \
     -DCMAKE_C_FLAGS="-O3 -fPIC -pthread -march=native -mtune=native" \
     -DCMAKE_CXX_FLAGS="-O3 -fPIC -pthread -march=native -mtune=native" \
-    -DPYTHON_EXECUTABLE:FILEPATH=$(which python) \
+    -DPYTHON_EXECUTABLE:FILEPATH="$(command -v python)" \
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
-    -DCMAKE_INSTALL_PREFIX="$prefix" \
+    -DCMAKE_INSTALL_PREFIX="$prefixCompile" \
     -DFFTW_ROOT="$FFTWPATH" \
     ..
 
 make -j$P
 make install
+
+# TODO: auto python version
+export PYTHONPATH="$(realpath $prefixCompile/lib/python*/site-packages):$PYTHONPATH"
 
 python -c 'from toast.tests import run; run()'
